22854d07843d482fa41cc3dd20ca95c39d888b61
Your Name <pclenovo780@gmail.com>
2026-06-10 20:35:28 +0530

    modified:   app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -56,7 +56,7 @@ export default function Home() {
       {/* Background */}
       <div className="absolute inset-0">
         <div
-          className="absolute inset-0 opacity-20 bg-fit-cover bg-center"
+          className="absolute inset-0 opacity-60 bg-fit-cover bg-center"
           style={{
             backgroundImage:
               "url('https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=2000&auto=format&fit=crop')",
